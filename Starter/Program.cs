c1331ee6a77332cbbb70b272a390f3e6c8cd3e4a
danielhagos11 <138770966+danielhagos11@users.noreply.github.com>
2026-06-12 16:41:00 +0100

    Update student scores and grading logic

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -1,17 +1,19 @@
-﻿// initialize variables - graded assignments 
-int currentAssignments = 5;
 
-int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
-int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
-int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+int examAssignments = 5;
 
-// Student names
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
 int[] studentScores = new int[10];
 
-// Write the Report Header to the console
+string currentStudentLetterGrade = "";
+
+// display the header row for scores/grades
+Console.Clear();
 Console.WriteLine("Student\t\tGrade\n");
 
 foreach (string name in studentNames)
@@ -30,19 +32,56 @@ foreach (string name in studentNames)
     else if (currentStudent == "Logan")
         studentScores = loganScores;
 
-    // initialize/reset the sum of scored assignments
     int sumAssignmentScores = 0;
-
-    // initialize/reset the calculated average of exam + extra credit scores
     decimal currentStudentGrade = 0;
+    int gradedAssignments = 0;
 
+    /* 
+    the inner foreach loop sums assignment scores
+    extra credit assignments are worth 10% of an exam score
+    */
     foreach (int score in studentScores)
     {
-        // add the exam score to the sum
-        sumAssignmentScores += score;
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+            sumAssignmentScores += score;
+        else
+            sumAssignmentScores += score / 10;w
     }
 
-    currentStudentGrade = (decimal)(sumAssignmentScores) / currentAssignments;
+    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
 
-    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t?");
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+    else
+        currentStudentLetterGrade = "F";
+
+    
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
 }
+
+Console.WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
